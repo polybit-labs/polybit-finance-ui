1e7b6ac6c5e534188c910c86c811b130a148a5e7
Matt Leeburn <dev@leeburn.co>
2022-10-10 14:09:46 +1100

    Added Connect Wallet page

--- src/components/pages/ConnectWallet.tsx
+++ src/components/pages/ConnectWallet.tsx
@@ -2,13 +2,47 @@ import React from 'react'
 import { Button } from '../Button'
 import "./ConnectWallet.css"
 import Footer from './Footer'
+import {
+    useAccount,
+    useNetwork,
+    useBalance,
+    useConnect,
+    useDisconnect,
+    useEnsAvatar,
+    useEnsName,
+} from "wagmi"
 
 export interface ConnectWalletProps {
     returnPath: string
 }
 
+export interface walletConnector {
+    ready: boolean,
+    id: string
+}
+
 function ConnectWallet({ returnPath }: ConnectWalletProps) {
-    const thing = returnPath
+    const { address, connector, isConnected } = useAccount()
+    const { chain, chains } = useNetwork()
+    const { data } = useBalance({
+        addressOrName: address,
+    })
+    const { data: ensName } = useEnsName({ address })
+    const { connect, connectors, error, isLoading, pendingConnector } =
+        useConnect()
+    const { disconnect } = useDisconnect({
+        onError(error) {
+            console.log('Error', error)
+        }
+    })
+
+    const coinbaseConnector = connectors[0]
+    const metamaskConnector = connectors[1]
+    const walletConnectConnector = connectors[2]
+    console.log(coinbaseConnector)
+    console.log(isConnected)
+    console.log(connector?.id)
+
     return (
         <>
             <div className="title-section">
@@ -24,18 +58,50 @@ function ConnectWallet({ returnPath }: ConnectWalletProps) {
             <div className="connect-provider">
                 <div className="connect-provider-box">
                     <div><img className="coinbase-logo" src={require("../../assets/images/coinbase-logo.png")} alt="Connect to Coinbase"></img></div>
-                    <div><Button text="Use Coinbase" type="button" onClick="" buttonSize="btn--large" buttonStyle="btn--filled" /></div>
+                    <div><button className="connect-wallet-button-primary" disabled={!coinbaseConnector.ready}
+                        key={coinbaseConnector.id}
+                        onClick={() => connect({ connector: coinbaseConnector })}>{coinbaseConnector.id != connector?.id &&
+                            "Use Coinbase"}
+                        {!coinbaseConnector.ready && ' (unsupported)'}
+                        {isLoading &&
+                            coinbaseConnector.id === pendingConnector?.id &&
+                            ' (connecting)'}
+                        {coinbaseConnector.id === connector?.id &&
+                            'Connected'}</button>
+                    </div>
                 </div>
                 <div className="connect-provider-box">
                     <div>
                         <img className="metamask-logo" src={require("../../assets/images/metamask-logo.png")} alt="Connect to MetaMask"></img></div>
-                    <div><Button text="Use MetaMask" type="button" onClick="" buttonSize="btn--large" buttonStyle="btn--primary" /></div>
+                    <div>
+                        <button className="connect-wallet-button-secondary" disabled={!metamaskConnector.ready}
+                            key={metamaskConnector.id}
+                            onClick={() => connect({ connector: metamaskConnector })}>{metamaskConnector.id != connector?.id &&
+                                "Use MetaMask"}
+                            {!metamaskConnector.ready && ' (unsupported)'}
+                            {isLoading &&
+                                metamaskConnector.id === pendingConnector?.id &&
+                                ' (connecting)'}
+                            {metamaskConnector.id === connector?.id &&
+                                'Connected'}</button>
+                    </div>
                 </div>
                 <div className="connect-provider-box">
                     <div>
                         <img className="walletconnect-logo" src={require("../../assets/images/walletconnect-logo.png")} alt="Connect to WalletConnect"></img>
                     </div>
-                    <div><Button text="Use WalletConnect" type="button" onClick="" buttonSize="btn--large" buttonStyle="btn--primary" /></div>
+                    <div>
+                        <button className="connect-wallet-button-secondary" disabled={!walletConnectConnector.ready}
+                            key={walletConnectConnector.id}
+                            onClick={() => connect({ connector: walletConnectConnector })}>{walletConnectConnector.id != connector?.id &&
+                                "Use WalletConnect"}
+                            {!walletConnectConnector.ready && ' (unsupported)'}
+                            {isLoading &&
+                                walletConnectConnector.id === pendingConnector?.id &&
+                                ' (connecting)'}
+                            {walletConnectConnector.id === connector?.id &&
+                                'Connected'}</button>
+                    </div>
                 </div>
             </div>
             <div className="connect-provider-notice"><p>Wallets are provided by external providers and by selecting you agree to Terms of those Providers.</p></div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,135 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+/* import { Buffer } from 'buffer';
+
+// @ts-ignore
+window.Buffer = Buffer; */
+
+import {
+  Chain,
+  WagmiConfig,
+  createClient,
+  configureChains,
+} from 'wagmi'
+
+//import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { publicProvider } from 'wagmi/providers/public'
+import { infuraProvider } from 'wagmi/providers/infura'
+import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
+
+const bscTestnet: Chain = {
+  id: 97,
+  name: 'BSC Testnet',
+  network: 'bsc',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'BNB',
+    symbol: 'BNB',
+  },
+  rpcUrls: {
+    default: 'http://data-seed-prebsc-1-s2.binance.org:8545/',
+  },
+  testnet: true,
+}
+
+const mainnet: Chain = {
+  id: 56,
+  name: "Binance Smart Chain",
+  network: "BSC",
+  rpcUrls: { default: "https://bsc-dataseed.binance.org/" },
+  nativeCurrency: {
+    name: "Binance Coin",
+    symbol: "BNB",
+    decimals: 18
+  },
+  blockExplorers: {
+    default:
+    {
+      name: "bscscan.com",
+      url: "https://bscscan.com"
+    }
+  }
+};
+
+const testnet: Chain = {
+  id: 97, //97 // 0x61
+  name: "Binance Smart Chain Testnet",
+  network: "BSC",
+  rpcUrls: { default: "https://data-seed-prebsc-1-s1.binance.org:8545/" },
+  testnet: true,
+  nativeCurrency: {
+    name: "Binance Coin Testnet",
+    symbol: "tBNB",
+    decimals: 18
+  },
+  blockExplorers: {
+    default:
+    {
+      name: "bscscan.com",
+      url: "https://testnet.bscscan.com"
+    }
+  }
+};
+
+const bscMainFork: Chain = {
+  id: 5777,
+  name: "Binance Smart Chain Testnet",
+  network: "BSC",
+  rpcUrls: { default: "HTTP://127.0.0.1:8545" },
+  testnet: true,
+  nativeCurrency: {
+    name: "BCS Fork",
+    symbol: "BNB",
+    decimals: 18
+  },
+};
+
+const chains = [mainnet, testnet, bscMainFork];
+const infuraId = process.env.INFURA_ID;
+
+const { provider, webSocketProvider } = configureChains(chains, [
+  publicProvider(),
+  infuraProvider({ apiKey: infuraId }),
+  /* jsonRpcProvider({
+    rpc: (chain) => {
+      if (chain.id !== bscTestnet.id) return null
+      return { http: chain.rpcUrls.default }
+    }
+  } )*/
+])
+
+// Set up client
+const client = createClient({
+  autoConnect: true,
+  connectors: [
+    new CoinbaseWalletConnector({
+      chains,
+      options: {
+        appName: "Polybit Finance",
+      },
+    }),
+    new MetaMaskConnector({ chains }),
+    new WalletConnectConnector({
+      chains,
+      options: {
+        qrcode: true,
+      },
+    })
+  ],
+  provider,
+  webSocketProvider,
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <WagmiConfig client={client}>
     <App />
-  </React.StrictMode>
+  </WagmiConfig>
 );
 
