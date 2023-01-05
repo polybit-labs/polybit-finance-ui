08c9125c602253445e71c5231d7198eb2aa6748c
Matt Leeburn <dev@leeburn.co>
2023-01-05 19:32:28 +1100

    added support for bsc testnet

--- src/components/api/GetOwnedAssets.tsx
+++ src/components/api/GetOwnedAssets.tsx
@@ -6,12 +6,14 @@ import apiURL from "./api-info.json"
 export const GetOwnedAssets = (detfAddress: string) => {
     const [response, setResponse] = useState<Array<any>>()
     const network = useNetwork()
+    const { chain } = useNetwork()
+    const chainId: string = chain ? chain.id.toString() : ""
     const rpc = network.chain?.rpcUrls.default
     let isLoading: boolean
     let isSuccess: boolean
 
     useEffect(() => {
-        axios.post(apiURL["apiURL"] + "/api/get_owned_assets", { "rpc_provider": rpc, "detf_address": detfAddress })
+        axios.post(apiURL["apiURL"] + "/api/get_owned_assets", { "rpc_provider": rpc, "chain_id": chainId, "detf_address": detfAddress })
             .then(res => {
                 setResponse(res.data)
             })
