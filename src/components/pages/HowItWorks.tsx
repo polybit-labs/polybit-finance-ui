e5ae0cf224c6dac0e84a1405bbee676d488710ea
Matt Leeburn <dev@leeburn.co>
2023-03-29 12:22:37 +1100

    implemented responsive design for how it works page

--- src/components/pages/HowItWorks.tsx
+++ src/components/pages/HowItWorks.tsx
@@ -19,19 +19,136 @@ const HowItWorks = () => {
     return (
         <><TitleContainer title={title} />
             <SubTitleContainer info={subTitle} />
-            <div className="how-it-works">
-                <div className="how-it-works-container">
-                    <h2>1. Themed investing</h2>
-                    <p>Assets are grouped into familiar themes, which reduces the research and discovery process to find exciting and legitimate decentralised projects.</p>
-                    <br />
-                    <h2>2. Risk management</h2>
-                    <p>DETFs are automatically rebalanced every 90 days to manage risk as the market moves. Each DETF is rebalanced by a different weighting such as total liquidity, total market cap or equally balanced.</p>
-                    <br />
-                    <h2>3. DEX aggregation</h2>
-                    <p>Polybit’s Decentralised Exchange (DEX) aggregation algorithm, leverages over $3.5 billion of liquidity pools to ensure both available liquidity and best price for efficient digital asset trades.</p>
-                    <br />
-                    <h2>4. Self-custodial wallet extension</h2>
-                    <p>Each DETF is a self-custodial smart contract, connected to and controlled by your wallet. Assets are never pooled or centralised and their ownership can be proven on the blockchain at any time.</p>
+            <div className="how-it-works-container">
+                <div className="how-it-works-container-desktop">
+                    <div className="dark-grey-section">
+                        <div className="dark-grey-section-container">
+                            <div className="desktop-dark-grey-section-content">
+                                <div className="dark-grey-section-image">
+                                    <img className="section-one-image" src={require("../../assets/images/how-it-works-section-one-image.png")}></img>
+                                </div>
+                                <div className="dark-grey-section-text">
+                                    <div className="dark-grey-section-text-header">
+                                        <img className="text-header-icon" src={require("../../assets/images/how-it-works-section-one-icon.png")}></img>
+                                        <div className="text-header-title">Thematic investing</div>
+                                    </div>
+                                    <div className="dark-grey-section-text-description">
+                                        <p>Assets are grouped into familiar themes, which reduces the research and discovery process to find exciting and legitimate decentralised projects.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="mobile-dark-grey-section-content">
+                                <div className="dark-grey-section-image">
+                                    <img className="section-one-image" src={require("../../assets/images/how-it-works-section-one-image.png")}></img>
+                                </div>
+                                <div className="dark-grey-section-text">
+                                    <div className="dark-grey-section-text-header">
+                                        <img className="text-header-icon" src={require("../../assets/images/how-it-works-section-one-icon.png")}></img>
+                                        <div className="text-header-title">Thematic investing</div>
+                                    </div>
+                                    <div className="dark-grey-section-text-description">
+                                        <p>Assets are grouped into familiar themes, which reduces the research and discovery process to find exciting and legitimate decentralised projects.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="light-grey-section">
+                        <div className="light-grey-section-container">
+                            <div className="desktop-light-grey-section-content">
+                                <div className="light-grey-section-text">
+                                    <div className="light-grey-section-text-header">
+                                        <img className="text-header-icon" src={require("../../assets/images/how-it-works-section-two-icon.png")}></img>
+                                        <div className="text-header-title">Risk management</div>
+                                    </div>
+                                    <div className="light-grey-section-text-description">
+                                        <p>DETFs are automatically rebalanced every 90 days to manage risk as the market moves. Each DETF is rebalanced by a different weighting such as total liquidity, total market cap or equally balanced.</p>
+                                    </div>
+                                </div>
+                                <div className="light-grey-section-image">
+                                    <img className="section-two-image" src={require("../../assets/images/how-it-works-section-two-image.png")}></img>
+                                </div>
+                            </div>
+                            <div className="mobile-light-grey-section-content">
+                                <div className="light-grey-section-image">
+                                    <img className="section-two-image" src={require("../../assets/images/how-it-works-section-two-image.png")}></img>
+                                </div>
+                                <div className="light-grey-section-text">
+                                    <div className="light-grey-section-text-header">
+                                        <img className="text-header-icon" src={require("../../assets/images/how-it-works-section-two-icon.png")}></img>
+                                        <div className="text-header-title">Risk management</div>
+                                    </div>
+                                    <div className="light-grey-section-text-description">
+                                        <p>DETFs are automatically rebalanced every 90 days to manage risk as the market moves. Each DETF is rebalanced by a different weighting such as total liquidity, total market cap or equally balanced.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="dark-grey-section">
+                        <div className="dark-grey-section-container">
+                            <div className="desktop-dark-grey-section-content">
+                                <div className="dark-grey-section-image">
+                                    <img className="section-three-image" src={require("../../assets/images/how-it-works-section-three-image.png")}></img>
+                                </div>
+                                <div className="dark-grey-section-text">
+                                    <div className="dark-grey-section-text-header">
+                                        <img className="text-header-icon" src={require("../../assets/images/how-it-works-section-three-icon.png")}></img>
+                                        <div className="text-header-title">DEX aggregation</div>
+                                    </div>
+                                    <div className="dark-grey-section-text-description">
+                                        <p>Polybit’s Decentralised Exchange (DEX) aggregation algorithm, leverages over $3.5 billion of liquidity pools to ensure both available liquidity and best price for efficient digital asset trades.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="mobile-dark-grey-section-content">
+                                <div className="dark-grey-section-image">
+                                    <img className="section-three-image" src={require("../../assets/images/how-it-works-section-three-image.png")}></img>
+                                </div>
+                                <div className="dark-grey-section-text">
+                                    <div className="dark-grey-section-text-header">
+                                        <img className="text-header-icon" src={require("../../assets/images/how-it-works-section-three-icon.png")}></img>
+                                        <div className="text-header-title">DEX aggregation</div>
+                                    </div>
+                                    <div className="dark-grey-section-text-description">
+                                        <p>Polybit’s Decentralised Exchange (DEX) aggregation algorithm, leverages over $3.5 billion of liquidity pools to ensure both available liquidity and best price for efficient digital asset trades.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="light-grey-section">
+                        <div className="light-grey-section-container">
+                            <div className="desktop-light-grey-section-content">
+                                <div className="light-grey-section-text">
+                                    <div className="light-grey-section-text-header">
+                                        <img className="text-header-icon" src={require("../../assets/images/how-it-works-section-four-icon.png")}></img>
+                                        <div className="text-header-title">Self-custodial</div>
+                                    </div>
+                                    <div className="light-grey-section-text-description">
+                                        <p>Each DETF is a self-custodial smart contract, connected to and controlled by your wallet. Assets are never pooled or centralised and their ownership can be proven on the blockchain at any time.</p>
+                                    </div>
+                                </div>
+                                <div className="light-grey-section-image">
+                                    <img className="section-four-image" src={require("../../assets/images/how-it-works-section-four-image.png")}></img>
+                                </div>
+                            </div>
+                            <div className="mobile-light-grey-section-content">
+                                <div className="light-grey-section-image">
+                                    <img className="section-four-image" src={require("../../assets/images/how-it-works-section-four-image.png")}></img>
+                                </div>
+                                <div className="light-grey-section-text">
+                                    <div className="light-grey-section-text-header">
+                                        <img className="text-header-icon" src={require("../../assets/images/how-it-works-section-four-icon.png")}></img>
+                                        <div className="text-header-title">Self-custodial</div>
+                                    </div>
+                                    <div className="light-grey-section-text-description">
+                                        <p>Each DETF is a self-custodial smart contract, connected to and controlled by your wallet. Assets are never pooled or centralised and their ownership can be proven on the blockchain at any time.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <Footer />
